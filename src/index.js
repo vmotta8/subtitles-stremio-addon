697fbac5796f426420753655ce002d4321432c02
Vinicius Motta <viniciusmotta8@gmail.com>
2021-02-01 15:54:32 -0300

    feat: add count words function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const fs = require('fs')
 // const words = require('./words/words.json')
-let subtitle = fs.readFileSync('/Users/vmotta8/repositories/subtitles-translator-ts/src/subtitle/Interstellar.2014.720p.BluRay.x264.YIFY.srt', 'utf-8')
+const subtitle = fs.readFileSync('/Users/vmotta8/repositories/subtitles-translator-ts/src/subtitle/Interstellar.2014.720p.BluRay.x264.YIFY.srt', 'utf-8')
 
 function stringToArray (str) {
   return str.toLocaleLowerCase().match(/[a-zA-Z']+/g)
 }
 
-subtitle = stringToArray(subtitle)
+function countWords (words) {
+  const index = {}
 
-console.log(subtitle)
+  words.forEach(function (word) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (!(index.hasOwnProperty(word))) {
+      index[word] = 0
+    }
+    index[word]++
+  })
+
+  return index
+}
+
+const subtitleArr = stringToArray(subtitle)
+const subtitleCounted = countWords(subtitleArr)
+
+console.log(subtitleCounted)
