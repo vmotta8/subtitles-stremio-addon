ba1b3354954f60abd49e6b28ad2057c766409547
Vinicius Motta <viniciusmotta8@gmail.com>
2021-04-02 17:19:41 -0300

    feat: add addon js

---
+++ addon.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable new-cap */
+const { addonBuilder, serveHTTP, publishToCentral } = require('stremio-addon-sdk')
+const { generateSubtitle } = require('./src/generateSubtitle')
+
+const builder = new addonBuilder({
+  id: 'org.subtitletranslateaddon',
+  version: '1.0.0',
+
+  name: 'English Portuguese Br Subtitles',
+
+  types: ['movie'],
+  catalogs: [],
+  resources: [
+    'subtitles'
+  ]
+})
+
+builder.defineSubtitlesHandler(async function (args) {
+  const subtitles = await generateSubtitle(args.id)
+  return Promise.resolve({ subtitles: subtitles })
+})
+
+serveHTTP(builder.getInterface(), {
+  port: 7000,
+  static: '/public'
+})
+
+// If you want this addon to appear in the addon catalogs, call .publishToCentral() with the publically available URL to your manifest
+// publishToCentral('https://my-addon.com/manifest.json')
